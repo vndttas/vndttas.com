8f84f7b6ad279fd9347fea64b45af8c1d9184ba6
vndttas <135358347+vndttas@users.noreply.github.com>
2023-09-03 22:01:12 +0700

    Add files via upload

---
+++ Commercial/script.js
@@ -0,0 +1,17 @@
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.querySelector(".background video");
+    video.play();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.querySelector(".content");
+    video.play();
+});
+
+
+
+
+
+
+</script>
